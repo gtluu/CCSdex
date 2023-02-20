83520802c55730d809e764fa5ac24073e9303976
unknown <gtluu912@gmail.com>
2023-02-20 12:42:01 -0800

    scorched earth on old files

--- CCSdex/views.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, url_for, redirect, request, session
-from CCSdex import app, CCSdexSearch
-
-
-@app.route('/')
-def ccsdex():
-    form = CCSdexSearch(request.form)
-    return render_template('ccsdex.html', title='CCSdex', form=form)
